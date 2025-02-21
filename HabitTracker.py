c33a9d4a3c76701f617b4b0443f1967caa271ca8
Manisha0909-coder <mannu91069@gmail.com>
2025-02-21 14:30:33 +0530

    Added sqlalchemy database

--- HabitTracker.py
+++ HabitTracker.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import Dict
-from datetime import date
+from datetime import date,timedelta
+from sqlalchemy.orm import Session
+from database import sessionLocal, Base, engine,HabitDB
+
+
+#Dependency to get DB session
+def get_db():
+    db= sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
@@ -13,7 +24,7 @@ class Habit(BaseModel):
     progress: Dict[str,bool]={}
 
 # In-memory database (dict for simplicity)
-habits: Dict[int, Habit] = {}
+#habits: Dict[int, Habit] = {}
 
 habit_id_counter = 1
 
@@ -22,48 +33,87 @@ def home():
     return {"message": "Welcome to the Habit Tracker API!"}
 
 @app.post("/habits/")
-def create_habit(habit: Habit):
-    global habit_id_counter
-    habits[habit_id_counter] = habit
-    habit_id_counter += 1
-    return {"message": "Habit added successfully", "habit_id": habit_id_counter - 1}
+def create_habit(habit: Habit, db:Session = Depends(get_db)):
+    new_habit = HabitDB(
+        name = habit.name,
+        description = habit.description,
+        completed = habit.completed,
+    )
+    db.add(new_habit)
+    db.commit()
+    db.refresh(new_habit)
+    
+    
+    #global habit_id_counter
+    #habits[habit_id_counter] = habit
+    #habit_id_counter += 1
+    return {"message": "Habit added successfully", "habit_id": new_habit.id}
 
+#Get single Habit
 @app.get("/habits/{habit_id}")
-def get_habit(habit_id: int):
-    if habit_id not in habits:
+def get_habit(habit_id: int,db: Session =Depends(get_db)):
+    habit = db.query(HabitDB).filter(HabitDB.id == habit_id).first()
+    if not habit:
         raise HTTPException(status_code=404, detail="Habit not found")
-    return habits[habit_id]
+    return habit
 
+#Get all habits
 @app.get("/habits/")
-def get_all_habits():
+def get_all_habits(db: Session = Depends(get_db)):
+    habits = db.query(HabitDB).all()
     return habits
 
+#Update Habit
 @app.put("/habits/{habit_id}")
-def update_habit(habit_id: int, habit: Habit):
-    if habit_id not in habits:
+def update_habit(habit_id: int, habit:Habit, db: Session = Depends(get_db)):
+    db_habit = db.query(HabitDB).filter(HabitDB.id == habit_id).first()
+    if not db_habit:
         raise HTTPException(status_code=404, detail="Habit not found")
-    habits[habit_id] = habit
+    
+    db_habit.name = habit.name
+    db_habit.description = habit.description
+    db_habit.completed = habit.completed
+    db.commit()
+
     return {"message": "Habit updated successfully"}
 
 @app.delete("/habits/{habit_id}")
-def delete_habit(habit_id: int):
-    if habit_id not in habits:
+def delete_habit(habit_id: int, db:Session = Depends(get_db)):
+    habit = db.query(HabitDB).filter(HabitDB.id == habit_id).first()
+    if not habit:
         raise HTTPException(status_code=404, detail="Habit not found")
-    del habits[habit_id]
+    db.delete(habit)
+    db.commit()
     return {"message": "Habit deleted successfully"}
 
 @app.post("/habits/{habit_id}/completed")
-def habit_completed(habit_id:int):
-    if habit_id not in habits:
+def habit_completed(habit_id:int, db: Session = Depends(get_db)):
+    habit = db.query(HabitDB).filter(HabitDB.id == habit_id).first()
+    if not habit:
         raise HTTPException(status_code=404, detail="Habit not found")
     
-    today = str(date.today())
-    habits[habit_id].progress[today]=True
+    #today = str(date.today())
+    #habits[habit_id].progress[today]=True
+
+    today = date.today()
+    if habit.last_completed_date == today:
+        return{"message":f"Habit {habit_id} already marked today"}
+    
+    if habit.last_completed_date == today - timedelta(days=1):
+        habit.streak+=1 #Increment 
+    else:
+        habit.streak=1 #Reset the streak if skipped the day
+    
+    habit.last_completed_date = today
+    db.commit()
     return{"message":f"Habit {habit_id} marked completed today"}
 
-@app.get("/habits/{habit_id}/history")
-def get_habithistory(habit_id:int):
-    if habit_id not in habits:
+
+@app.get("/habits/{habit_id}/progress")
+def get_habit_progress(habit_id:int,db:Session = Depends(get_db)):
+    habit =db.query(HabitDB).filter(HabitDB.id == habit_id).first()
+
+    if not habit:
         raise HTTPException(status_code=404, detail="Habit not found")
         #raise HTTPException(status_code=400, detail="Invalid request")
-    return habits[habit_id].progress
+    return {"habit_id":habit.id,"name":habit.name,"streak": habit.streak,"last_completed_date":habit.last_completed_date}
---
+++ database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Date
+from sqlalchemy.orm import sessionmaker, declarative_base
+from datetime import date
+
+DATABASE_URL = "sqlite:///./habits.db"
+
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+sessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base = declarative_base()
+
+
+#Habit base model
+class HabitDB(Base):
+    __tablename__="habits"
+
+    id = Column(Integer,primary_key=True,index=True)
+    name = Column(String,index=True)
+    description = Column(String, index=True)
+    completed = Column(Boolean, default= False)
+    #last_completed_date = Column(Date, nullable=True)
+    created_date = Column(Date, default=date.today)
+
+    #Progress Tracking
+    streak = Column(Integer, default=0)
+    last_completed_date = Column(Date, nullable=True)
+# Create table
+# Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
